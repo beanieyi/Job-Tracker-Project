215f47861a9365bea875e4395162acc29744a631
Robin <robin.christopher.yi@gmail.com>
2025-01-22 13:16:50 -0700

    Adjusted some animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -102,8 +102,8 @@ function ApplicationView({ applications }) {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-          duration: 0.5,
-          delay: 0.2,
+          duration: 0.6,
+          delay: 0.3,
           ease: [0, 0.71, 0.2, 1.01],
       }}
     >
